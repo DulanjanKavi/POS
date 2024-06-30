125b2455ae22d49f01c0829f258e6a8c15e29be6
DulanjanKavi <dulanjankavindu@outlook.com>
2024-06-30 12:28:23 +0530

    add check detail component

--- src/component/checkDetailComponenet.tsx
+++ src/component/checkDetailComponenet.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+
+
+
+
 
 function checkDetailComponenet() {
+    const [checkDetails,setCheckDetails]=useState([])
+
+    const getCheckDetails=async()=>{
+        try{
+            const result=await window.REPORT_API.getCheckDetails();
+            if(result!=null)
+              {
+                setCheckDetails(result)
+              } 
+        }catch(error){
+            console.error()
+        }
+      }  
+      useEffect(() => {
+        getCheckDetails();
+      }, []);
+
+
+
     return (
         <div className='border-l-2 border-black h-screen m-1'>
           <div className="bg-white p-1 rounded-lg">
@@ -8,14 +31,14 @@ function checkDetailComponenet() {
         Collected Check Details
     </div>
     
-        <div className='mx-8'>
+        <div className='mx-4'>
         <table className="table-auto w-full">
                         <tbody>
                             <tr className="flex  justify-between border-b-2 text-xl  ">
                                 
-                                <td className='w-w/3   '>Check no.</td>
-                                <td className='w-w/3  pr- '>Amount</td>
-                                <td className='w-w/3 hidden md:block '>Date</td>
+                                <td className='   '>Check no.</td>
+                                <td className='  '>Amount</td>
+                            
                                 
                                 
                                 
@@ -25,15 +48,15 @@ function checkDetailComponenet() {
         </div>
         </div>
         <div className='max-h-[70vh] text-lg p-2 m-2  overflow-y-auto '>
-        <div className="w-ful">
-        {/*{checkDetails.map((item, index) => (
-            <div key={index} className="flex justify-between  p-2 m-1 mx-4 border rounded-lg hover:bg-gray-200 relative group">
-                <div className='w-w/3 pl-4'>{item.checkNumber}</div>
-                <div className='w-w/3'>{item.amount}</div>
-                <div className='w-w/3 hidden md:block'>{item.date}</div>
-            </div>
-        ))}*/}
-        </div>
+        <div className="w-full">
+  {checkDetails.map((item, index) => (
+    <div key={index} className="flex justify-between   border-b-2">
+      <div className=' '>{checkDetails[index].aditionalDetails}</div>
+      <div className=''>{(checkDetails[index].total).toFixed(2)}</div>
+    </div>
+  ))}
+</div>
+
     </div>
     
     
