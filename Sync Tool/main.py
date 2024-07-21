2d25674096d86872a94ac0ec3bb8e8fb07de6421
Namindu <namindu@live.com>
2024-07-21 22:08:05 +0530

    added test codes

--- Sync Tool/main.py
+++ Sync Tool/main.py
@@ -17,10 +17,12 @@ def process_queue():
     # Fetch actions with status 'pending'
     cursor.execute("SELECT rowid, action, data FROM queue WHERE status='pending'")
     rows = cursor.fetchall()
+    print(rows)
 
     for row in rows:
         rowid, action, data = row
-        data = json.loads(data)
+        if (data.startswith("{") and data.endswith("}")) or (data.startswith("[") and data.endswith("]")):
+            data = json.loads(data)
 
         try:
             # Dynamically import the function
@@ -43,10 +45,22 @@ def process_queue():
 
     conn.close()
 
+# Function to add an example action to the queue
+def add_ex():
+    current_directory = os.getcwd()
+    db_path = os.path.join(current_directory, '..', 'pos.db')
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO queue (action, data, status) VALUES ('print_word', '{\"word\":\"World\"}', 'pending')")
+    conn.commit()
+    conn.close()
+
 # Function to run the process_queue function every five minutes
 def run_scheduler():
     while True:
         process_queue()
+        add_ex()
+        add_ex()
         time.sleep(30)  # Sleep for 300 seconds (5 minutes)
 
 if __name__ == "__main__":
