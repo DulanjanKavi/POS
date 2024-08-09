11ca8e216b2593005ab27cdc50d0af728d671279
unknown <jkktg001@gmail.com>
2024-08-09 18:58:38 +0530

    2024/08/09

--- Sync Tool/functions/get_all_products.py
+++ Sync Tool/functions/get_all_products.py
@@ -1,14 +1,11 @@
 import requests
-import json
+import sqlite3
+import os
 from config import BASE_URL
 
 def get_all_products(data):
     # URL of the API endpoint
     url = f'{BASE_URL}/get-products?status={data["status"]}'
-
-    # Commented-out part to fetch JWT token from the system
-    # with open('/path/to/jwt/token/file', 'r') as f:
-    #     jwt_token = f.read().strip()
 
     # Hardcoded JWT token for testing
     jwt_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmFjNzNlZmM4ODVmYmMwYzM4MWYyZWYiLCJmaXJzdF9uYW1lIjoiZGVtbzIiLCJsYXN0X25hbWUiOiJkZW0iLCJlbWFpbCI6ImRlbW9kZW1AZ21haWwuY29tIiwicm9sZSI6IjY2OWUwNDNjOGJkMTUxMmQwNzgzMjBjZSIsImNvbXBhbnkiOiI2NmFjNjk3MmM4ODVmYmMwYzM4MWYyN2IiLCJpYXQiOjE3MjMyMDQ4ODksImV4cCI6MTcyNDY0NDg4OX0.8SKzLx8abqUXopON7Z02ekle2DGPm_cELSFc8It7tak'
@@ -24,11 +21,34 @@ def get_all_products(data):
     if response.status_code == 200:
         products = response.json()
 
-        # Save the company info to a .js file
-        with open('all_products.js', 'w') as f:
-            f.write(f'const products = {json.dumps(products, indent=4)};\n')
-            f.write('module.exports = products;\n')
-        
-        print("Products has been saved to all_products.js")
+        # Insert products into the items table
+        current_directory = os.getcwd()
+        db_path = os.path.join(current_directory, '..', 'pos.db')
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        for product in products:
+            # Convert lists to comma-separated strings
+            barcodes = ', '.join(product['barcodes'])
+            prices = ', '.join(product['prices'])
+
+            cursor.execute('''
+                INSERT INTO items (snumber, name, SKU, thumbnail, inventry_type, IS_active, price, discount)
+                VALUES (?, ?, ?, ?, ?, ?, ?, 0)
+            ''', (
+                product['id'],
+                product['name'],
+                product['sku'],
+                product['thumbnail'],
+                product['inventory_type'],
+                product['is_active'],
+                # barcodes,  # Comma-separated string
+                prices     # Comma-separated string
+            ))
+
+        conn.commit()
+        conn.close()
+
+        print("Products have been successfully saved to the database.")
     else:
-        print(f"Failed to fetch company information. Status code: {response.status_code}, Message: {response.text}")
+        print(f"Failed to fetch products. Status code: {response.status_code}, Message: {response.text}")
